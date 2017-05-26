6a5813092595a7baddecb2880e0d10148ed2f54e
Francisco <jose.francisco.tw@gmail.com>
2017-05-26 15:46:49 -0300

    changed plot radiation pattern in order to save only one image with every radiating pattern on it

--- simulaciones/plotRadiationPattern/plotRadiationPattern.py
+++ simulaciones/plotRadiationPattern/plotRadiationPattern.py
@@ -13,14 +13,14 @@ def set_plot_environment(plt, title, y_label, x_label, locc=None):
     plt.ylabel(y_label)
     plt.xlabel(x_label)
     plt.grid(True)
-
+    plt.legend()
     if locc is not None:
-        plt.legend(loc=locc)
+        plt.legend(loc=locc, prop={'size':18})
 
 
 def save_plots(filename, plt, path='../../written/thesis/Chapter3/Figs/Raster'):
-        plt.tight_layout()
-        plt.savefig(os.path.join(path, filename + ".png"), bbox_inches='tight')
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, filename + ".png"), bbox_inches='tight')
 
 
 def find_nearest(array, value):
@@ -29,11 +29,11 @@ def find_nearest(array, value):
 
 def read_data(dirname):
     f0 = 2434262820.514000 # in Hz
-    length = 30 # in cm
+    length = 32.5 # in cm
     data = []
     x = []
     for filename in sorted(os.listdir(dirname)):
-        print(filename)
+        # print(filename)
         with open(os.path.join(dirname, filename)) as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
             while next(spamreader)[0] != "Values":
@@ -41,34 +41,36 @@ def read_data(dirname):
             d = tuple(zip(*spamreader))
         data.append(max(tuple(map(float, d[1]))))
         x.append(np.arcsin(float(re.sub('.*_', '', filename))/length)*180/np.pi)
+        print(np.arcsin(float(re.sub('.*_', '', filename))/length)*180/np.pi, max(tuple(map(float, d[1]))))
         # index = find_nearest(np.array(list(map(float, d[0]))), f0)
         # data.append(d[1][index])
 
     return x, data
 
 
-def plot(fig, name, x, y):
+def plot(fig, name, save_images, x, y):
     # x = np.array(list(map(lambda x: float(x)/ 1e9, x)))
     # e_max = find_nearest(x, 2.8)
     # e_min = find_nearest(x, 2.001)
-    plt.figure(fig)
+    # plt.figure(fig, figsize=(8.0, 5.0))
 
     if x:
-        plt.plot(list(map(lambda y: -y, x[::-1])) + x, y[::-1] + y, linewidth=2)
+        plt.plot(list(map(lambda y: -y, x[::-1])) + x, y[::-1] + y, linewidth=2, label=name)
     else:
         plt.plot(y[::-1] + y, linewidth=2)
     # plt.plot(x[e_min:e_max], y[e_min:e_max], linewidth=2)
-    set_plot_environment(plt, 'Transmitted Power ' + name, 'Power [dBm]', 'Angle [deg]')
-    save_plots(name, plt)
+    set_plot_environment(plt, 'Transmitted Power ' + name, 'Power [dBm]', 'Angle [deg]', locc=4)
+    # if save_images:
+    #     save_plots(name, plt)
 
 def main():
     plt.rcParams.update({'font.size': 20})
-    
-    plot(1, 'HH_pattern', *read_data("HH"))
-    plot(2, 'HV_pattern', *read_data("VH"))
-    plot(3, 'VH_pattern', *read_data("VH"))
-    plot(4, 'VV_pattern', *read_data("VV"))
-    # plt.show()
+    plt.figure(figsize=(9, 8))
+    save_images = False
+    plot(1, 'HH pattern', save_images, *read_data("HH"))
+    plot(1, 'HV pattern', save_images, *read_data("VH"))
+    plot(1, 'VV pattern', save_images, *read_data("VV"))
+    save_plots('patterns', plt)
 
 
 if __name__ == '__main__':
