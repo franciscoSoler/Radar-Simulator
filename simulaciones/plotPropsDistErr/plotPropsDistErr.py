14cce9d538d8424c339a4d5ed3bd754ace0faf06
Francisco <jose.francisco.tw@gmail.com>
2017-11-19 20:04:39 -0300

    added figure resources for the writting

--- simulaciones/plotPropsDistErr/plotPropsDistErr.py
+++ simulaciones/plotPropsDistErr/plotPropsDistErr.py
@@ -31,13 +31,29 @@ def save_plots(filename, plt, path='../../written/thesis/Chapter5/Figs/Raster'):
     plt.savefig(os.path.join(path, filename + ".png"), bbox_inches='tight', dpi=80)
 
 
-def plot(name, xaxis, yaxis, save_images, x, y, legends=None, locc=None):
+def plot(title, name, xaxis, yaxis, save_images, x, y, legends=None, locc=None):
     plt.figure(figsize=(9, 7))
     lineObjects = plt.plot(x, list(zip(*y)), linewidth=2)
 
 
-    set_plot_environment(plt, 'Gananancia medida a diferentes distancias', yaxis, xaxis, lineObjects, legends, locc=locc)
+    set_plot_environment(plt, title + ' medida a diferentes distancias', yaxis, xaxis, lineObjects, legends, locc=locc)
 
+    if save_images:
+        save_plots(name, plt)
+    else:
+        plt.show()
+
+
+def plot_incert(title, name, xaxis, yaxis, save_images, x, y, legends=None, locc=None):
+    plt.figure(figsize=(9, 7))
+    lineObjects = plt.plot(x, list(zip(*y)), linewidth=2)
+
+
+    set_plot_environment(plt, title + ' medida a diferentes distancias', yaxis, xaxis, lineObjects, legends, locc=locc)
+    lim_x = 9.5
+    lim_y = 30
+    plt.plot([lim_x, lim_x], [0, lim_y], 'k-')
+    plt.plot([0, lim_x], [lim_y, lim_y], 'k-')
     if save_images:
         save_plots(name, plt)
     else:
@@ -51,19 +67,19 @@ def main():
 
     distances = [30760, 35000, 37760.1695, 40000]
     data = []
-    data.append([0 , 0.9974998783 , -0.8999873159 , 0.9970854521 , 5.0829022535 , 0.9974244573 , 0.0563536612 , 0.9991246271 , 1.1021289469])
-    data.append([5 , 0.996851467 , -28.5141663823 , 0.9965158111 , -22.5305974732 , 0.9968962677 , -27.5567038267 , 0.9986251585 , -26.5105696718])
-    data.append([10 , 0.9962033718 , -56.1283462497 , 0.9959464141 , -50.1440980009 , 0.9963682879 , -55.1697621157 , 0.9981258771 , -54.1232690916])
-    data.append([40 , 0.9923214343 , 138.1865577226 , 0.9925351551 , 144.1748820091 , 0.9932048122 , 139.1518713272 , 0.9951341194 , 140.2005175668])
-    data.append([100 , 0.984591606 , 166.8162791476 , 0.9857389337 , 172.8127555094 , 0.986900466 , 167.7950516932 , 0.9891707856 , 168.8480043635])
+    data.append(list(map(lambda x: round(x,1), [0 , 0.9974998783 , -0.8999873159 , 0.9970854521 , 5.0829022535 , 0.9974244573 , 0.0563536612 , 0.9991246271 , 1.1021289469])))
+    data.append(list(map(lambda x: round(x,1), [5 , 0.996851467 , -28.5141663823 , 0.9965158111 , -22.5305974732 , 0.9968962677 , -27.5567038267 , 0.9986251585 , -26.5105696718])))
+    data.append(list(map(lambda x: round(x,1), [10 , 0.9962033718 , -56.1283462497 , 0.9959464141 , -50.1440980009 , 0.9963682879 , -55.1697621157 , 0.9981258771 , -54.1232690916])))
+    data.append(list(map(lambda x: round(x,1), [40 , 0.9923214343 , -221.8134422774, 0.9925351551 , -215.8251179909, 0.9932048122 , -220.8481286728, 0.9951341194 , -219.7994824332])))
+    data.append(list(map(lambda x: round(x,1), [100 , 0.984591606 , -553.1837208524, 0.9857389337 , -547.1872444906, 0.986900466 , -552.2049483068, 0.9891707856 , -551.1519956365])))
 
     d = list(zip(*data))
     delta = d[0]
     gain = d[1::2]
     phase = d[2::2]
 
-    plot('deltaDistGain', 'Delta [m]', 'Ganancia [dB]', save_images, delta, gain, legends=distances, locc=1)
-    plot('deltaDistPhase', 'Delta [m]', 'Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
+    plot('Ganancia', 'deltaDistGain', 'Delta [m]', 'Ganancia [dB]', save_images, delta, gain, legends=distances, locc=1)
+    plot('Fase', 'deltaDistPhase', 'Delta [m]', 'Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
     
     data = []
     data.append([0 , 0.00 , 0.00 , 0.00 , 0.00 , 0.00 , 0.00 , 0.00 , 0.00])
@@ -74,7 +90,7 @@ def main():
     d = list(zip(*data))
     delta = d[0]
     phase = d[2::2]
-    plot('incertDistPhase', 'Delta [m]', 'Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
+    plot_incert('Incertidumbre de fase', 'incertDistPhase', 'Delta [m]', 'Delta Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
 
     distances = [2.201, 2.229, 2.255]
     data = []
@@ -88,8 +104,8 @@ def main():
     gain = d[1::2]
     phase = d[2::2]
 
-    plot('deltaDistGainRadar', 'Delta [mm]', 'Ganancia [dB]', save_images, delta, gain, legends=distances, locc=1)
-    plot('deltaDistPhaseRadar', 'Delta [mm]', 'Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
+    plot('Ganancia', 'deltaDistGainRadar', 'Delta [mm]', 'Ganancia [dB]', save_images, delta, gain, legends=distances, locc=1)
+    plot('Fase', 'deltaDistPhaseRadar', 'Delta [mm]', 'Fase [deg]', save_images, delta, phase, legends=distances, locc=2)
 
 
 if __name__ == '__main__':
